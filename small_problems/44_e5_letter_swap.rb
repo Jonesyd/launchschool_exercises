753750c78491c69203c60ee1221b2f49947ec723
Craig Jones <craigjonesy@hotmail.com>
2018-04-13 13:39:15 +1000

    exercises

--- small_problems/44_e5_letter_swap.rb
+++ small_problems/44_e5_letter_swap.rb
@@ -1,37 +1,25 @@
+# first attempt
 
-
-p swap('Oh what a wonderful day it is') #== 'hO thaw a londerfuw yad ti si'
-p swap('Abcde') #== 'ebcdA'
-p swap('a') #== 'a'
-
-=begin
-PEDAC
-problem
-  Given a string of words separated by spaces,
-  write a method that takes this string of words
-  returns a string in which the first and last letters
-  of every word is swapped.
-
-  You may assume that every word contains at least one letter,
-  that the string will always contain at least one word.
-  You may also assume that each string contains nothing
-  but words and spaces
-Examples (test case above)
-
-Data Structures
-  input string
-  array map ?
-  output string
-Algorithm
-  string split into words
-  iterate over words (for transformation)
-  each word transform map back
-  return new string
-
-  string.split.map do |word| # array of words
-    word[-1] if word[0]
-    word[0] if word[-1]
+def swap(string)
+  new_array = string.split.map do |word|
+    first_letter = word.slice(0)
+    last_letter = word.slice(-1)
+    word.slice!(0)
+    word.insert(0, last_letter)
+    word.slice!(-1)
+    word.insert(-1, first_letter)
   end
+  new_array.join(" ")
+end
 
+test_one = 'Oh what a wonderful day it is'
+test_two = 'Abcde'
+test_three = 'a'
 
-=end
+p swap(test_one) == 'hO thaw a londerfuw yad ti si'
+p swap(test_two) == 'ebcdA'
+p swap(test_three) == 'a'
+
+p 'Oh what a wonderful day it is' == test_one
+p 'Abcde' == test_two
+p 'a' == test_three
