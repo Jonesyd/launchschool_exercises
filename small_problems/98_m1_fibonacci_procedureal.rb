a179ad6d4bc6567123b18f0dcbcc88ddb12f6366
Craig Jones <craigjonesy@hotmail.com>
2018-04-23 17:38:37 +1000

    update

---
+++ small_problems/98_m1_fibonacci_procedureal.rb
@@ -0,0 +1,16 @@
+def fibonacci(integer)
+  array = []
+  array.fill(0, integer + 1) do |index|
+    case index
+    when 0 then array[0] = 0
+    when 1 then array[1] = 1
+    else array[index] = array[index-2] + array[index-1]
+    end
+  end
+  array[integer]
+end
+
+p fibonacci(5) == 5
+p fibonacci(20) == 6765
+p fibonacci(100) == 354224848179261915075
+p fibonacci(100_001) # => 4202692702.....8285979669707537501
