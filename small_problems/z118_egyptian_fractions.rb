41a5d6ae8befb15c760c7dd4e6dbe5b84d0f2ebc
craig jones <craigjonesy@hotmail.com>
2018-05-02 17:03:30 +1000

    maths like an egyptian

--- small_problems/z118_egyptian_fractions.rb
+++ small_problems/z118_egyptian_fractions.rb
@@ -1,80 +1,33 @@
-def egyptian(int_a, int_b) #method 1
-
+def egyptian(rational) #method 1
+  array = []
   counter = 1
-  until Rational(int_a, int_b) == Rational(rational)
-    rational = Rational(int_a, int_b)
+  running_total = 0
 
+  while running_total < rational
 
+    if Rational(1, counter) <= (rational - running_total)
+      array << counter
+      running_total += Rational(1, counter)
+    end
 
     counter += 1
   end
-
-
-
-
-  # include one if it is less than the first number
-  # if not skip
-  # include two if first num + current num < current num
-  # create a new fraction each time.
-  # i.e. subract one from the current denominator and numerator
-  # numerator always equals one
+  array
 end
 
-egyptian(Rational(2, 1))    # -> [1, 2, 3, 6]
-egyptian(Rational(137, 60)) # -> [1, 2, 3, 4, 5]
-egyptian(Rational(3, 1))    # -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 230, 57960]
+def unegyptian(array)
+  array.map { |num| Rational(1, num) }.sum
+end
 
-# unegyptian(egyptian(Rational(1, 2))) == Rational(1, 2)
-# unegyptian(egyptian(Rational(3, 4))) == Rational(3, 4)
-# unegyptian(egyptian(Rational(39, 20))) == Rational(39, 20)
-# unegyptian(egyptian(Rational(127, 130))) == Rational(127, 130)
-# unegyptian(egyptian(Rational(5, 7))) == Rational(5, 7)
-# unegyptian(egyptian(Rational(1, 1))) == Rational(1, 1)
-# unegyptian(egyptian(Rational(2, 1))) == Rational(2, 1)
-# unegyptian(egyptian(Rational(3, 1))) == Rational(3, 1)
+p egyptian(Rational(2, 1))    == [1, 2, 3, 6]
+p egyptian(Rational(137, 60)) == [1, 2, 3, 4, 5]
+p egyptian(Rational(3, 1))    == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 230, 57960]
 
-=begin
-PEDAC
-problem
-  An Egyptian Fraction is the sum of a series of distinct unit fractions (no two are the same), such as:
-
-  Write two methods:
-    one that takes a Rational number as an argument, and
-    returns an Array of the denominators that are part of an Egyptian Fraction representation of the number, and
-
-    another that takes an Array of numbers in the same format, and calculates the resulting Rational number.
-
-  You will need to use the Rational class provided by Ruby.
-
-Examples (test cases) above
-Data Structure
-  input int int
-  variables and build array?
-  output array
-
-Algorithm
-  use Relational#denominator
-  use Relational#numerator
-  def egyptian(int_a, int_b) #method 1
-
-    counter = x
-    until relational(int_a, int_b) == relational(numerator, denominator)
-
-      counter += 1
-    end
-
-    # iterate up until .. infinity
-    # start at one.
-    # include one if it is less than the first number
-    # if not skip
-    # include two if first num + current num < current num
-    # create a new fraction each time.
-    # i.e. subract one from the current denominator and numerator
-
-
-  end
-
-
-
-
-=end
+p unegyptian(egyptian(Rational(1, 2))) == Rational(1, 2)
+p unegyptian(egyptian(Rational(3, 4))) == Rational(3, 4)
+p unegyptian(egyptian(Rational(39, 20))) == Rational(39, 20)
+p unegyptian(egyptian(Rational(127, 130))) == Rational(127, 130)
+p unegyptian(egyptian(Rational(5, 7))) == Rational(5, 7)
+p unegyptian(egyptian(Rational(1, 1))) == Rational(1, 1)
+p unegyptian(egyptian(Rational(2, 1))) == Rational(2, 1)
+p unegyptian(egyptian(Rational(3, 1))) == Rational(3, 1)
