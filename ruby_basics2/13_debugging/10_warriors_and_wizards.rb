d1120b556b2d74285441bf2e00f50f148ab789c8
Craig <craigjonesy@hotmail.com>
2019-04-17 04:51:50 +1000

    debugging exercises

---
+++ ruby_basics2/13_debugging/10_warriors_and_wizards.rb
@@ -0,0 +1,24 @@
+require "pry-byebug"
+
+player = { strength: 10, dexterity: 10, charisma: 10, stamina: 10 }
+
+character_classes = {
+  warrior:  { strength:   20 },
+  thief:    { dexterity:  20 },
+  scout:    { stamina:    20 },
+  mage:     { charisma:   20 }
+}
+
+input = ""
+
+loop do
+  puts "Please type your class (warrior, thief, scout, mage):"
+  input = gets.chomp.downcase.to_sym
+  break if character_classes.keys.include?(input)
+  puts "please select a valid class"
+end
+
+player.merge!(character_classes[input])
+
+puts "Your character stats:"
+puts player
