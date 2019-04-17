5eed755fbbfd945f6a25df36c0727e9d9231f93e
Craig <craigjonesy@hotmail.com>
2019-04-17 18:44:57 +1000

    documentation exercises

---
+++ small_problems2/01_Documentation_Again/01_class_and_instance_methods.rb
@@ -0,0 +1,4 @@
+# File::path - returns the string representation of the path.
+# File#path - returns the pathname used to create the file.
+
+# one is an class method, and one is an instance (of the class) method
