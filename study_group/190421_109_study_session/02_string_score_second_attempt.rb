f92d38232b1e80f94babe1ee3c57b635d4edb43a
Craig <craigjonesy@hotmail.com>
2019-04-22 14:26:21 +1000

    repeat exercise

--- study_group/190421_109_study_session/02_string_score_second_attempt.rb
+++ study_group/190421_109_study_session/02_string_score_second_attempt.rb
@@ -7,32 +7,29 @@
 # Examples (happy path, edgecases, failures):
   # see below
 
-def high(string)
-  score_hash = score
-  result_hash = {}
-  words_array = string.split
-  words_array.each do |word|
-    score = 0
-    letter_array = word.chars
-    letter_array.each { |letter| score += score_hash[letter.to_sym] }
-    result_hash[word.to_sym] = score
-  end
-  max_value = result_hash.values.max
-  result_hash.key(max_value).to_s
-end
-
-def score
-  start_letter = "a"
-  start_number = 1
+def score_hash
+  letter = "a"
   score_hash = {}
-
-  loop do
-    score_hash[start_letter.to_sym] = start_number
-    start_letter = start_letter.succ
-    start_number += 1
-    break if start_number > 26
+  (1..26).each do |num|
+    score_hash[letter] = num
+    letter = letter.succ
   end
   score_hash
+end
+
+def result_hash(string, score_hash)
+  result_hash = {}
+  string.split.each do |word|
+    score = 0
+    word.chars.each { |char| score += score_hash[char] }
+    result_hash[word] = score
+  end
+  result_hash
+end
+
+def high(string)
+  result_hash = result_hash(string, score_hash)
+  result_hash.key(result_hash.values.max)
 end
 
 p high('man i need a taxi up to ubud') == 'taxi'
