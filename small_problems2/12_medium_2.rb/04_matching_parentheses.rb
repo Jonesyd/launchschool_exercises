3b3620b6bbcf99e89364edc98b6b1e0509379dee
Craig <craigjonesy@hotmail.com>
2019-04-28 22:07:10 +1000

    exercises

---
+++ small_problems2/12_medium_2.rb/04_matching_parentheses.rb
@@ -0,0 +1,15 @@
+def balanced?(string)
+  new_string = string.delete("^()")
+  new_string.size.times { new_string.gsub!(/\(\)/, "") }
+  new_string.empty?
+end
+
+p balanced?('What (is) this?') == true
+p balanced?('What is) this?') == false
+p balanced?('What (is this?') == false
+p balanced?('((What) (is this))?') == true
+p balanced?('((What)) (is this))?') == false
+p balanced?('Hey!') == true
+p balanced?(')Hey!(') == false
+p balanced?('What ((is))) up(') == false
+balanced?('What ()))((()') == false
