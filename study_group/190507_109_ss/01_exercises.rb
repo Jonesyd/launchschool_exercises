84871fd04c38ef2828cc2bbebf1333122e31859c
Craig <craigjonesy@hotmail.com>
2019-05-08 18:55:58 +1000

    study exercise

--- study_group/190507_109_ss/01_exercises.rb
+++ study_group/190507_109_ss/01_exercises.rb
@@ -3,28 +3,24 @@
 # Replace every letter in the string with the 3rd letter following it in the alphabet (i.e. c becomes f, Z becomes C).
 
 # Then return this modified string.
-require "pry-byebug"
-
-#wip
 
 def letter_changes(string)
   array = ("a".."z").to_a
 
-  result = string.chars.map do |char|
-    binding.pry
+  result_array = string.chars.map do |char|
     case
-    when array.index(char) == 24 then "a"
-    when array.index(char) == 25 then "b"
-    when array.index(char) == 26 then "c"
-    when char.match(/[a-z]/i) then array[array.index(char) + 3]
+    when char.downcase == "x" then char == "x" ? "a" : "A"
+    when char.downcase == "y" then char == "y" ? "b" : "B"
+    when char.downcase == "z" then char == "z" ? "c" : "C"
+    when char.match(/[a-z]/) then array[array.index(char.downcase) + 3]
+    when char.match(/[A-Z]/) then array[array.index(char.downcase) + 3].upcase
     else char
     end
   end
-  result.join
+  result_array.join
 end
 
 
-
-p letter_changes("this long cake@&") #== "wklv orqj fdnh@&"
-p letter_changes("Road trip9") #== "Urdg wuls9"
-p letter_changes("EMAILZ@gmail.com") #== "HPDLOC@jpdlo.frp"
+p letter_changes("this long cake@&") == "wklv orqj fdnh@&"
+p letter_changes("Road trip9") == "Urdg wuls9"
+p letter_changes("EMAILZ@gmail.com") == "HPDLOC@jpdlo.frp"
