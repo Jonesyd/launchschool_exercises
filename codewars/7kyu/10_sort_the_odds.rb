98ccae85dedb52319559522b609cbe8c080ed809
Craig <craigjonesy@hotmail.com>
2019-05-11 14:44:23 +1000

    exercises

---
+++ codewars/7kyu/10_sort_the_odds.rb
@@ -0,0 +1,39 @@
+# You have an array of numbers.
+# Your task is to sort ascending odd numbers but even numbers must be on their places.
+#
+# Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+#
+# Example
+
+# input array
+#  create a new array by iterate over the array using select
+#  sort array !
+#  counter = -1
+#  map original array
+#  if num.odd?
+#    counter + 1
+#    sort array[counter]
+#  else num
+#
+# output array
+
+def sort_array(array)
+  odd_array = array.select { |num| num.odd? }.sort!
+  counter = -1
+
+  array.map do |num|
+    if num.odd?
+      counter += 1
+      odd_array[counter]
+    else
+      num
+    end
+  end
+
+end
+
+
+
+p sort_array([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
+p sort_array([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
+p sort_array([]) == []
