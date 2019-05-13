c4fc0d756718823c44a24139ae0b9a106cc978b5
Craig <craigjonesy@hotmail.com>
2019-05-13 21:46:31 +1000

    exercises

---
+++ small_problems3/01_easy1/07_string_strings.rb
@@ -0,0 +1,8 @@
+def stringy(integer)
+  Array.new(integer) { |index| index.even? ? "1" : "0"  }.join
+end
+
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
