ff136cf098a913f6b33112765f2271e92522ebd2
Craig <craigjonesy@hotmail.com>
2019-11-14 11:17:06 +1100

    exercises

---
+++ oo_problems/04_easy_1/02_what_output.rb
@@ -0,0 +1,29 @@
+class Pet
+  attr_reader :name
+
+  def initialize(name)
+    @name = name.to_s
+  end
+
+  def to_s
+    "My name is #{@name.upcase}."
+  end
+end
+
+# name = "Fluffy"
+# fluffy = Pet.new(name)
+# puts fluffy.name
+# puts fluffy
+# puts fluffy.name
+# puts name
+# puts fluffy
+
+name = 42
+fluffy = Pet.new(name)
+name += 1
+puts fluffy.name
+puts fluffy
+puts fluffy.name
+puts name
+
+# reason: when name is converted to string it is a different object.
